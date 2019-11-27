2c2c3c5c92839baecbfdd21a5dcca2d8772ecc40
Thanatos91 <fhachemi@lerebours.org>
2019-11-27 14:51:28 +0100

    Ajoutez des fichiers projet.

---
+++ GestionnaireBDD/GstBdd.cs
@@ -0,0 +1,63 @@
+﻿using ClassesMetier;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace GestionnaireBDD
+{
+    public class GstBdd
+    {
+        MySqlConnection cnx;
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+
+        public GstBdd()
+        {
+            string driver = "server=localhost;user id=root;password=;database=gestionsalles";
+            cnx = new MySqlConnection(driver);
+            cnx.Open();
+        }
+
+        public List<Manifestation> GetAllManifestations()
+        {
+            List<Manifestation> lesManifs = new List<Manifestation>();
+            cmd = new MySqlCommand("SELECT * FROM manifestation", cnx);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Manifestation uneManif = new Manifestation()
+                {
+                    IdManif = Convert.ToInt32(dr[0].ToString()),
+                    NomManif = dr[1].ToString(),
+                    DateDebutManif = dr[2].ToString(),
+                    DateFinManif = dr[3].ToString(),
+                    //LaSalle
+                    //Latitude = Convert.ToDouble(dr[4].ToString()),
+                    //Longitude = Convert.ToDouble(dr[5].ToString())
+                };
+                lesManifs.Add(uneManif);
+            }
+            dr.Close();
+            return lesManifs;
+
+
+
+            return null;
+        }
+
+        public List<Place> GetAllPlacesByIdManifestation(int idManif,int idSalle)
+        {
+            return null;
+        }
+
+        public List<Tarif> GetAllTarifs()
+        {
+            return null;
+        }
+
+        public void ReserverPlace(int idPlace, int idSalle,int idManif)
+        {
+            
+        }
+    }
+}
---
+++ UWPGestionSalles/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using GestionnaireBDD;
+using ClassesMetier;
+using Windows.UI.Popups;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPGestionSalles
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btnReserver_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void lstManifs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private async void gvPlaces_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+    }
+}
